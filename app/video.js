dc66e89c4246d4f3248cbc1b0628faa9f363fb0e
jdm79 <james.malvern@gmail.com>
2017-02-25 00:20:54 +0100

    add autoplay when visible

--- app/video.js
+++ app/video.js
@@ -1,29 +1,48 @@
-var videos = document.getElementsByTagName("video"),
-fraction = 0.8;
+var video = document.getElementById('video'),
+    info = document.getElementById('info'),
+    fraction = 0.8;
+
 function checkScroll() {
+  var x = video.offsetLeft,
+      y = video.offsetTop,
+      w = video.offsetWidth,
+      h = video.offsetHeight,
+      r = x + w, //right
+      b = y + h, //bottom
+      visibleX,
+      visibleY,
+      visible;
 
-    for(var i = 0; i < videos.length; i++) {
+  if (window.pageXOffset >= r ||
+      window.pageYOffset >= b ||
+      window.pageXOffset + window.innerWidth < x ||
+      window.pageYOffset + window.innerHeight < y
+     ) {
 
-        var video = videos[i];
+    info.innerHTML = '0%';
+    return;
+  }
 
-        var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
-            b = y + h, //bottom
-            visibleX, visibleY, visible;
+  visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+  visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
 
-            visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
-            visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+  visible = visibleX * visibleY / (w * h);
 
-            visible = visibleX * visibleY / (w * h);
+  info.innerHTML = Math.round(visible * 100) + '%';
 
-            if (visible > fraction) {
-                video.play();
-            } else {
-                video.pause();
-            }
-
-    }
+  if (visible > fraction) {
+    video.play();
+  } else {
+    video.pause();
+  }
 
 }
 
 window.addEventListener('scroll', checkScroll, false);
 window.addEventListener('resize', checkScroll, false);
+
+//one time at the beginning, in case it starts in view
+checkScroll();
+
+//as soon as we know the video dimensions
+video.addEventListener('loadedmetadata', checkScroll, false);
